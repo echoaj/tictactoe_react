eb0b32020578b91bd4b94cac1e3e13c16ba5cc65
Alex Joslin <aljoslin13@yahoo.com>
2021-08-16 11:27:35 -0700

    Finished Timetravel

--- src/index.js
+++ src/index.js
@@ -59,55 +59,34 @@ class Board extends React.Component {
 
     // 1
     // Creates Board states (Array & next) to set up game
+    /*
     constructor(props){
         super(props);
         this.state = {
             squares: Array(9).fill(null),
             xIsNext:true,
         };
-    }
+    }*/
 
-    // 4
-    handleClick(i){
-        const squares = this.state.squares.slice();     // slice creates new copy of array
-        // End Game Early is Player Has Won
-        if (calculateWinner(squares) || squares[i]){
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares:squares,
-            xIsNext:!this.state.xIsNext,
-        });
-    }
     
     // 3
     renderSquare(i) {
         return (
         <Square 
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)}
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
         />); // value is a prop
     }
 
     // 2
     // Sets up board layout
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if(winner){
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
         return (
             // The language below is not HTML, it is JSX (A syntax extension to JavaScript.)
             // It produces react elements and it is the reccomended sytax for React.
             // Also is reccommended to place JSX in () to avoid automatic semicolon insertion.
             // JSX helps prevent XSS (Cross-Site-Scripting Attacks)
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                 {this.renderSquare(0)}
                 {this.renderSquare(1)}
@@ -130,15 +109,73 @@ class Board extends React.Component {
 
 // COMPONENT 3
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{squares:Array(9).fill(null)}],
+            stepNumber: 0,
+            xIsNext:true,
+        }
+    }
+
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+        xIsNext: (step % 2) ===0,
+        })
+    }
+
+    // 4
+    handleClick(i){
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();     // slice creates new copy of array
+        // End Game Early is Player Has Won
+        if (calculateWinner(squares) || squares[i]){
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to move #' + move : 'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={ () => this.jumpTo(move)}>{desc}</button>
+                </li>
+            )
+        })
+
+        let status;
+        if(winner){
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+
         return (
             <div className="game">
                 <div className="game-board">
-                <Board />
+                <Board 
+                    squares={current.squares}
+                    onClick={(i) => this.handleClick(i)}/>
                 </div>
                 <div className="game-info">
-                <div>{/* status */}</div>
-                <ol>{/* TODO */}</ol>
+                <div>{status}</div>
+                <ol>{moves}</ol>
                 </div>
             </div>
         );
