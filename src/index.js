8cf6e5ccd3353ec26ca4891ec9c0ae772a6f7a15
Alex Joslin <aljoslin13@yahoo.com>
2021-08-16 09:46:26 -0700

    Finished Winner Detection

--- src/index.js
+++ src/index.js
@@ -70,6 +70,10 @@ class Board extends React.Component {
     // 4
     handleClick(i){
         const squares = this.state.squares.slice();     // slice creates new copy of array
+        // End Game Early is Player Has Won
+        if (calculateWinner(squares) || squares[i]){
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares:squares,
@@ -89,7 +93,13 @@ class Board extends React.Component {
     // 2
     // Sets up board layout
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if(winner){
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             // The language below is not HTML, it is JSX (A syntax extension to JavaScript.)
@@ -134,6 +144,26 @@ class Game extends React.Component {
         );
     }
 }
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
 // ========================================
 
